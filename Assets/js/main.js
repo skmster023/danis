a08576b838f30058b6994094c1907e4a631708be
Edel <EG@Edels-MacBook-Air.local>
2017-01-26 22:46:52 -0500

    Dani

---
+++ Assets/js/main.js
@@ -0,0 +1,17 @@
+var container = document.getElementById("containerInfo");
+var btn = document.getElementById('btn');
+
+btn.addEventListener("click", function() {
+	var ourRequest = new XMLHttpRequest();
+	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');					
+	ourRequest.onload = function() {
+		var ourData = JSON.parse(ourRequest.responseText);
+		renderHTML(ourData);
+	};	
+	ourRequest.send();
+});
+
+function renderHTML(data) {
+	var htmlString = "I love that one too!!!";
+	container.insertAdjacentHTML('beforeend', htmlString);
+}
