2657ba92cd7c8e25a2366111cc4c5ac4e741d61b
Edel <EG@Edels-MacBook-Air.local>
2017-02-04 21:39:17 -0500

    adding Dani json

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,9 +1,9 @@
 var container = document.getElementById("containerInfo");
 var btn = document.getElementById('btn');
 
-btn.addEventListener("click", function() {
+btn.addEventListener("click", function()) {
 	var ourRequest = new XMLHttpRequest();
-	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');					
+	ourRequest.open('GET', 'danis/Assets/js/Dani.json');					
 	ourRequest.onload = function() {
 		var ourData = JSON.parse(ourRequest.responseText);
 		renderHTML(ourData);
@@ -12,6 +12,11 @@ btn.addEventListener("click", function() {
 });
 
 function renderHTML(data) {
-	var htmlString = "I love that one too!!!";
+	var htmlString = "";
+
+	for(i=0; i < data; i++) {
+		htmlString += "<p>Dani is a! " + data[i].attribute + "dog!</p>";
+ 	}
+	
 	container.insertAdjacentHTML('beforeend', htmlString);
 }
