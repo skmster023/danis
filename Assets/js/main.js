6b8c31141a7487d6d4f77bc02fd1567f70470786
Edel <EG@Edels-MacBook-Air.local>
2017-02-05 13:34:03 -0500

    limit

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -6,6 +6,7 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 if(counterClicks < 0) {
+	counterClicks++
 	btn.addEventListener("click", function() {
   		var ourRequest = new XMLHttpRequest();
   		ourRequest.open('GET', 'Assets/js/Dani.json');
