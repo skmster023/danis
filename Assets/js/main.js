d0e5e60d7b51f8e9ecbd58409fc062d90d1b7665
Edel <EG@Edels-MacBook-Air.local>
2017-02-05 11:29:48 -0500

    change web

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -3,7 +3,7 @@ var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
 	var ourRequest = new XMLHttpRequest();
-	ourRequest.open('GET', 'danis/Assets/js/Dani.json');					
+	ourRequest.open('GET', 'https://boiling-thicket-91212.herokuapp.com/danis/Assets/js/Dani.json');					
 	ourRequest.onload = function() {
 		var ourData = JSON.parse(ourRequest.responseText);
 		renderHTML(ourData);
@@ -14,7 +14,7 @@ btn.addEventListener("click", function() {
 function renderHTML(data) {
 	var htmlString = "";
 
-	for(i=0; i < data; i++) {
+	for(i=0; i<data; i++) {
 		htmlString += "<p>Dani is a! " + data[i].attribute + "dog!</p>";
  	}
 	
